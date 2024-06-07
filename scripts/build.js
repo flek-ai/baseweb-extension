843a5dcd51a0b492250a56da8adff104d93d926d
Ishan Sharma <ishan@flek.ai>
2024-06-07 18:03:21 +0530

    trying with esm format

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,7 @@ require('esbuild').build({
   entryPoints: ['lib/index.tsx'],
   outfile: 'dist/index.js',
   bundle: true,
+  format: 'esm',
   external: [
     ...Object.keys(pkg.peerDependencies || {})
   ],
